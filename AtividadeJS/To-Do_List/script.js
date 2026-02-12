7cb1aa74fe9f234ab9c6bd198baf1f9dfd710601
guilhermepadi86 <guilherme.siqueirapadi@gmail.com>
2026-02-12 09:43:01 -0300

    Atividade JS 2

--- AtividadeJS/To-Do_List/script.js
+++ AtividadeJS/To-Do_List/script.js
@@ -1,11 +1,26 @@
 let tarefa = document.getElementById("Tarefa")
+let lista = document.getElementById("Lista")
 
 AddTarefa.addEventListener('click', function(){
     adicionarTarefa(tarefa.value)
 })
 
+Remove.addEventListener('click', function(){
+    removerTarefa()
+})
+
 function adicionarTarefa(Task){
-    item = document.createElement("li")
-    item.appendChild(Task)
+    let item = document.createElement('li')
+    item.classList = 'ListaMenor'
+    item.textContent = Task
+    lista.appendChild(item)
 }
 
+function removerTarefa(){
+    if (lista.lastElementChild){
+        lista.removeChild(lista.lastElementChild)
+    }
+    else{
+        alert('Sem itens na lista para remover')
+    }
+}
