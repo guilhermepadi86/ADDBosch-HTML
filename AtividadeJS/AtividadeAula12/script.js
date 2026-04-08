0c56ab84e24117a66f22a876ecfd5826b702b1a6
guilhermepadi86 <guilherme.siqueirapadi@gmail.com>
2026-04-08 10:26:51 -0300

    Atividade JS Charts

--- AtividadeJS/AtividadeAula12/script.js
+++ AtividadeJS/AtividadeAula12/script.js
@@ -16,54 +16,263 @@ const vendas = [
   { id: 15, produto: "Mouse", categoria: "Eletrônicos", preco: 100, quantidade: 3, cliente: "João", cidade: "Curitiba", data: "2025-03-15" }
 ];
 
-const categorias = [...new Set(vendas.map((v) => v.categoria))]
+const faturamentoCategoria = vendas.reduce((acumulador, venda) => {
+  acumulador[venda.categoria] = (acumulador[venda.categoria] || 0) + (venda.preco * venda.quantidade);
+  return acumulador;
+}, {})
 
-const faturamentoTotal = vendas.map((v) => {
-  return {categoria: v.categoria, faturamento: v.preco * v.quantidade}
-})
-var valorE = 0
-var valorR = 0
-var valorC = 0
-var valorA = 0
-for (i=0; i < faturamentoTotal.length; i++) {
-  if (faturamentoTotal[i].categoria == 'Eletrônicos') {
-    valorE = valorE + faturamentoTotal[i].faturamento
+const quantidadeCategoria = vendas.reduce((acumulador, venda) => {
+  acumulador[venda.categoria] = (acumulador[venda.categoria] || 0) + venda.quantidade;
+  return acumulador;
+}, {})
+
+const faturamentoCidade = vendas.reduce((acumulador, venda) => {
+  acumulador[venda.cidade] = (acumulador[venda.cidade] || 0) + (venda.preco * venda.quantidade);
+  return acumulador;
+}, {})
+
+const quantidadeCidade = vendas.reduce((acumulador, venda) => {
+  acumulador[venda.cidade] = (acumulador[venda.cidade] || 0) + venda.quantidade;
+  return acumulador;
+}, {})
+
+const gastoCliente = vendas.reduce((acumulador, venda) => {
+  acumulador[venda.cliente] = (acumulador[venda.cliente] || 0) + (venda.quantidade * venda.preco)
+  return acumulador
+}, {})
+
+const distribuicaoCliente = vendas.reduce((acumulador, venda) => {
+  acumulador[venda.cliente] = (acumulador[venda.cliente] || 0) + venda.quantidade
+  return acumulador
+}, {})
+
+const quantidadeProduto = vendas.reduce((acumulador, venda) => {
+  acumulador[venda.produto] = (acumulador[venda.produto] || 0) + venda.quantidade
+  return acumulador
+}, {})
+
+const vendasMes = vendas.reduce((acumulador, venda) => {
+  acumulador[(venda.data).split("-")[1]] = (acumulador[(venda.data).split("-")[1]] || 0) + venda.quantidade
+  return acumulador
+}, {})
+
+for (i=0; i < Object.keys(vendasMes).length; i++) {
+  chaveAtual = Object.keys(vendasMes)[i]
+  console.log(chaveAtual)
+  switch (chaveAtual) {
+    case "01":
+      vendasMes['Janeiro'] = vendasMes[chaveAtual]
+      i = -1
+      delete vendasMes[chaveAtual]
+      break
+    case "02":
+      vendasMes['Fevereiro'] = vendasMes[chaveAtual]
+      i = -1
+      delete vendasMes[chaveAtual]
+      break
+    case "03":
+      vendasMes['Março'] = vendasMes[chaveAtual]
+      i = -1
+      delete vendasMes[chaveAtual]
+      break
+    case "04":
+      vendasMes['Abril'] = vendasMes[chaveAtual]
+      i = -1
+      delete vendasMes[chaveAtual]
+      break
+    case "05":
+      vendasMes['Maio'] = vendasMes[chaveAtual]
+      i = -1
+      delete vendasMes[chaveAtual]
+      break
+    case "06":
+      vendasMes['Junho'] = vendasMes[chaveAtual]
+      i = -1
+      delete vendasMes[chaveAtual]
+      break
+    case "07":
+      vendasMes['Julho'] = vendasMes[chaveAtual]
+      i = -1
+      delete vendasMes[chaveAtual]
+      break
+    case "08":
+      vendasMes['Agosto'] = vendasMes[chaveAtual]
+      i = -1
+      delete vendasMes[chaveAtual]
+      break
+    case "09":
+      vendasMes['Setembro'] = vendasMes[chaveAtual]
+      i = -1
+      delete vendasMes[chaveAtual]
+      break
+    case "10":
+      vendasMes['Outubro'] = vendasMes[chaveAtual]
+      i = -1
+      delete vendasMes[chaveAtual]
+      break
+    case "11":
+      vendasMes['Novembro'] = vendasMes[chaveAtual]
+      i = -1
+      delete vendasMes[chaveAtual]
+      break
+    case "12":
+      vendasMes['Dezembro'] = vendasMes[chaveAtual]
+      i = -1
+      delete vendasMes[chaveAtual]
+      break
   }
-  else if (faturamentoTotal[i].categoria == 'Roupas') {
-    valorR = valorR + faturamentoTotal[i].faturamento
-  }
-  else if (faturamentoTotal[i].categoria == 'Calçados') {
-    valorC = valorC + faturamentoTotal[i].faturamento
-  }
-  else if (faturamentoTotal[i].categoria == 'Acessórios') {
-    valorA = valorA + faturamentoTotal[i].faturamento
-  }
+  console.log(vendasMes)
 }
-const faturamentoPorCategoria = [valorE, valorR, valorC, valorA]
-
-
+console.log(vendasMes)
 
 const FaturamentoCategoriaBarras = document.getElementById('FaturamentoCategoria')
 const QuantidadeCategoriaPie = document.getElementById('QuantidadeCategoria')
+const FaturamentoCidadeBarras = document.getElementById('FaturamentoCidade')
+const PedidoCidadePie = document.getElementById('PedidoCidade')
+const GastoPorCliente = document.getElementById('GastoCliente')
+const DistribuicaoPorCliente = document.getElementById('DistribuicaoCliente')
+const QuantidadePorProduto = document.getElementById('QuantidadeProduto')
+const FaturamentoCategoriaPie = document.getElementById('FaturamentoCategoriaPie')
+const QuantidadeVSFaturamento = document.getElementById('QuantidadeFaturamentoCategoria')
+const VendasPorMes = document.getElementById('VendasMes')
 
+const options = {
+  scales: {
+    y: {
+      beginAtZero: true
+    }
+  }
+}
 
 new Chart(FaturamentoCategoriaBarras, {
   type: 'bar',
   data: {
-    labels: categorias,
+    labels: Object.keys(faturamentoCategoria),
     datasets: [{
-      labels: 'Faturamento',
-      data: faturamentoPorCategoria,
+      label: 'Faturamento por Categoria',
+      data: Object.values(faturamentoCategoria),
       borderWidth: 0.5
     }]
   },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
+  options: options
 })
 
+new Chart(QuantidadeCategoriaPie, {
+  type: 'pie',
+  data: {
+    labels: Object.keys(quantidadeCategoria),
+    datasets: [{
+      labels: 'Quantidade',
+      data: Object.values(quantidadeCategoria),
+      borderWidth: 0.5
+    }]
+  },
+  options: options
+})
 
+new Chart(FaturamentoCidadeBarras, {
+  type: 'bar',
+  data: {
+    labels: Object.keys(faturamentoCidade),
+    datasets: [{
+      label: 'Faturamento por Cidade',
+      data: Object.values(faturamentoCidade),
+      borderWidth: 0.5
+    }]
+  },
+  options: options
+})
+
+new Chart(PedidoCidadePie, {
+  type: 'pie',
+  data: {
+    labels: Object.keys(quantidadeCidade),
+    datasets: [{
+      labels: 'Pedidos',
+      data: Object.values(quantidadeCidade),
+      borderWidth: 0.5
+    }]
+  },
+  options: options
+})
+
+new Chart(GastoPorCliente, {
+  type: 'bar',
+  data: {
+    labels: Object.keys(gastoCliente),
+    datasets: [{
+      label: 'Gasto por Cliente',
+      data: Object.values(gastoCliente),
+      borderWidth: 0.5
+    }]
+  },
+  options: options
+})
+
+new Chart(DistribuicaoPorCliente, {
+  type: 'pie',
+  data: {
+    labels: Object.keys(distribuicaoCliente),
+    datasets: [{
+      labels: 'Quantidade',
+      data: Object.values(distribuicaoCliente),
+      borderWidth: 0.5
+    }]
+  },
+  options: options
+})
+
+new Chart(QuantidadePorProduto, {
+  type: 'bar',
+  data: {
+    labels: Object.keys(quantidadeProduto),
+    datasets: [{
+      label: 'Quantidade por Produto',
+      data: Object.values(quantidadeProduto),
+      borderWidth: 0.5
+    }]
+  },
+  options: options
+})
+
+new Chart(FaturamentoCategoriaPie, {
+  type: 'pie',
+  data: {
+    labels: Object.keys(faturamentoCategoria),
+    datasets: [{
+      labels: 'Faturamento',
+      data: Object.values(faturamentoCategoria),
+      borderWidth: 0.5
+    }]
+  },
+  options: options
+})
+
+new Chart(QuantidadeVSFaturamento, {
+  data: {
+    datasets: [{
+      type: 'bar',
+      label: 'Quantidade',
+      data: Object.values(quantidadeCategoria)
+    }, {
+      type: 'bar',
+      label: 'Faturamento',
+      data: Object.values(faturamentoCategoria)
+    }],
+    labels: Object.keys(faturamentoCategoria)
+  },
+  options: options
+})
+
+new Chart(VendasPorMes, {
+  type: 'bar',
+  data: {
+    labels: Object.keys(vendasMes),
+    datasets: [{
+      label: 'Quantidade por Produto',
+      data: Object.values(vendasMes),
+      borderWidth: 0.5
+    }]
+  },
+  options: options
+})
