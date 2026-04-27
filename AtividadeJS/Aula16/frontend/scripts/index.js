8a191162966156e202e2986c3ea8cc43dd93bb36
guilhermepadi86 <guilherme.siqueirapadi@gmail.com>
2026-04-27 11:16:01 -0300

    CRUD finalizado

--- AtividadeJS/Aula16/frontend/scripts/index.js
+++ AtividadeJS/Aula16/frontend/scripts/index.js
@@ -1,4 +1,5 @@
 const form = document.getElementById("form")
+const formatt = document.getElementById("formatt")
 
 form.addEventListener('submit', async (e) => {
     e.preventDefault()
@@ -24,6 +25,43 @@ form.addEventListener('submit', async (e) => {
     carregarUsuarios();
 })
 
+formatt.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    
+    const id = document.getElementById("idatt").value
+    const nome = document.getElementById("nomeatt").value
+    const email = document.getElementById("emailatt").value
+    const senha = document.getElementById("senhaatt").value
+    console.log(nome,email,senha)
+
+    const response = await fetch(`http://localhost:8080/atualizar/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email,
+            senha: senha
+        })
+    })
+
+    const data = await response.json();
+    console.log(data)
+    carregarUsuarios();
+})
+
+async function atualizarUsuario(id, nome, email, senha) {
+    const idatt = document.getElementById("idatt")
+    const nomeatt = document.getElementById("nomeatt")
+    const emailatt = document.getElementById("emailatt")
+    const senhaatt = document.getElementById("senhaatt")
+
+    idatt.value = id
+    nomeatt.value = nome
+    emailatt.value = email
+    senhaatt.value = senha
+}
 
 async function carregarUsuarios(){
     const response = await fetch('http://localhost:8080/usuarios');
@@ -38,7 +76,7 @@ async function carregarUsuarios(){
             <td>${user.email}</td>
             <td>
                 <button onclick="deletarUsuario(${user.id})">Deletar</button>
-                <button onclick="atualizarUsuario(${user.id}, ${user.nome}, ${user.email}, ${user.senha})">Atualizar</button>
+                <button onclick="atualizarUsuario('${user.id}', '${user.nome}', '${user.email}', '${user.senha}')">Atualizar</button>
             </td>
         `
         tbody.appendChild(tr)
